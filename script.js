69660714bb3d5ec5f2a63882e043b9fdab3528f4
Rahul <Rahulsawaratkar@gmail.com>
2023-08-15 20:52:27 +0530

    9th commit - 1st in release

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addTask() {
         const li = document.createElement("li");
         li.innerHTML = `
             <input type="checkbox">
-            <span>${input.value}</span>
+            <span class="task-text">${input.value}</span>
             <button class="task-remove" onclick="removeTask(this)">Remove</button>
         `;
         taskList.appendChild(li);
@@ -17,4 +17,25 @@ function addTask() {
 function removeTask(button) {
     const li = button.parentElement;
     li.remove();
+}
+
+function toggleComplete(checkbox) {
+    const taskText = checkbox.nextElementSibling;
+    if (checkbox.checked) {
+        taskText.style.textDecoration = "line-through";
+        taskText.style.color = "#aaa";
+    } else {
+        taskText.style.textDecoration = "none";
+        taskText.style.color = "#333";
+    }
+}
+
+function clearCompleted() {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            const li = checkbox.parentElement;
+            li.remove();
+        }
+    });
 }
